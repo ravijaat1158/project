9674964b6ff72e00a61cf930d1273a5a1a2c10ba
Ravi Lour <ravijaat2917@gmail.com>
2023-04-24 09:33:43 +0530

    adding layouts and installing some modules package

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "./Components/Layouts/Layout.js";
+
+function App() {
+    return (
+        <Layout>
+            <h1>Body</h1>
+        </Layout>
+  )
+}
+
+export default App;
