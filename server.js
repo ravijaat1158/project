e3a8e9d46208c88daaddaf9646ad8c8e33ea0437
Ravi Lour <ravijaat2917@gmail.com>
2023-05-04 10:11:47 +0530

    Ready For Build

--- server.js
+++ server.js
@@ -7,16 +7,28 @@ import Router from "./routes/getDataRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 // config middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(path.join(__dirname));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
 // Database connection
 connectDB();
 
 app.use("/api/v1", Router);
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server Listening on Port ${PORT}`);
